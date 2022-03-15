3dbea75d2d2fbf8ff6e5f4f4aa71101910a8f14e
Jesus Arriola <arriola.jesus@hotmail.com>
2022-03-15 08:04:29 -0600

    Add exercise fixed multi-resources

--- firstServer/instance.tf
+++ firstServer/instance.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "aws" {
-    access_key = "xxxxxxxxxxxxxxxxxx"
-    secret_key = "xxxxxxxxxxxxxxxxxxxxxxxx"
+    access_key = AWS_ACCESS_KEY_ID
+    secret_key = AWS_SECRET_ACCESS_KEY
     region = "us-east-2"
 }
 
