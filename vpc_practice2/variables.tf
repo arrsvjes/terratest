56005d613280fdb98737d097e7d3ec164b339d4d
Jesus Arriola <arriola.jesus@hotmail.com>
2022-03-22 13:45:02 -0600

    Exercise vpc added

---
+++ vpc_practice2/variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "192.168.0.0/20"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "vpc_practice1"
+}
+
+variable "subnet_cidr" {
+  type    = list(string)
+  default = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+}
+
+variable "subnet_name" {
+  type    = list(string)
+  default = ["subnet1a", "subnet1b", "subnet1c"]
+}
+
+variable "az" {
+  type    = list(string)
+  default = ["us-east-2a", "us-east-2b", "us-west-1a"]
+}
+
